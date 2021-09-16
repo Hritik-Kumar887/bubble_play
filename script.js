1408315850152ed9794a67e95d5e821d04b22740
hritikCodes <ritikkumar887@gmail.com>
2021-09-17 03:07:38 +0530

    made constillation

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ class Particle {
         this.y = mouse.y;
         // this.x = Math.random() * canvas.width;
         // this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 13 + 1;
+        this.size = Math.random() * 10 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
         this.color = 'hsl(' + hue + ', 100%, 50%)';
@@ -65,6 +65,20 @@ function handleParticles() {
     for (i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update();
         particlesArray[i].draw();
+        for(let j=i; j<particlesArray.length; j++){
+            const dx = particlesArray[i].x - particlesArray[j].x;
+            const dy = particlesArray[i].y - particlesArray[j].y;
+            const distance = Math.sqrt(dx*dx+dy*dy);
+            if(distance < 100){
+                ctx.beginPath();
+                ctx.strokeStyle = particlesArray[i].color;
+                ctx.lineWidth = 0.2;
+                ctx.moveTo(particlesArray[i].x,particlesArray[i].y);
+                ctx.lineTo(particlesArray[j].x,particlesArray[j].y);
+                ctx.stroke();
+                ctx.closePath();
+            }
+        }
         if (particlesArray[i].size <= 0.3) {
             particlesArray.splice(i, 1);
             i--;
@@ -73,9 +87,9 @@ function handleParticles() {
 }
 
 function animate() {
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'rgba(0,0,0,0.04)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // ctx.fillStyle = 'rgba(0,0,0,0.04)';
+    // ctx.fillRect(0, 0, canvas.width, canvas.height);
     handleParticles();
     hue+= 5;
     requestAnimationFrame(animate);
